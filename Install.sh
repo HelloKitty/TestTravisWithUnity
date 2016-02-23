f28994e18605a8f42e1076a99eb7cbe52cc3f898
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-23 15:08:09 -0600

    Updated Install for 5.3

--- Install.sh
+++ Install.sh
@@ -1,7 +1,29 @@
 #! /bin/sh
 
-echo 'Downloading from http://download.unity3d.com/download_unity/cc9cbbcc37b4/MacEditorInstaller/Unity-5.3.1f1.pkg: '
-curl -o Unity.pkg http://download.unity3d.com/download_unity/cc9cbbcc37b4/MacEditorInstaller/Unity-5.3.1f1.pkg
+# Based on https://github.com/vergenzt/ci-build/blob/unity-5.3/Scripts/install.sh which updates support for 5.3 component based installer.
 
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+BASE_URL=http://netstorage.unity3d.com/unity
+HASH=e87ab445ead0
+VERSION=5.3.1f1
+
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"

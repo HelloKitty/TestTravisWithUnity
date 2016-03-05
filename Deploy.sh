fe24ae32779f4bdb372c115f6d2224eb7a193396
Andrew Blakely <andrew.blakely@ymail.com>
2016-03-04 23:20:30 -0600

    Switched to Job_ID

--- Deploy.sh
+++ Deploy.sh
@@ -18,10 +18,10 @@ chmod +x /usr/local/Cellar/megatools/p7zip
 # Attach the build number to the build http://stackoverflow.com/questions/12643018/how-to-get-travis-ci-build-number-within-after-script-command
 cd ./Build/windows/
 ls
-7z a QABuild$(TRAVIS_BUILD_NUMBER).7z QABuild_data/ QABuild.exe
+7z a QABuild$((TRAVIS_JOB_ID - 1)).7z QABuild_data/ QABuild.exe
 cd ..
 cd ..
 ls
 
 # Push the single file with megaput it up to Mega.co
-megaput --path /Root -u unitytravisci@yahoo.com -p TravisUnityCI69 --no-ask-password QABuild$(TRAVIS_BUILD_NUMBER).7z
+megaput --path /Root -u unitytravisci@yahoo.com -p TravisUnityCI69 --no-ask-password QABuild$((TRAVIS_JOB_ID - 1)).7z
